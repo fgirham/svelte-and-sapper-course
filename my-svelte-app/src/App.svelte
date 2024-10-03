0cec31886c91adce2e2d098e1dcaed69bdaf55f1
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 14:31:25 +0700

    add name input and reset function

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,9 +1,34 @@
 <script>
 	import Navbar from "./sections/Navbar.svelte";
 
-	let disabled = false;
+	let name = "";
+
+	function onInputName(event) {
+		name = event.target.value;
+	}
+
+	function resetClicked() {
+		name = "";
+	}
 </script>
 
 <Navbar title="vCard Generator" />
 
-<button {disabled}>Reset</button>
+<form>
+	<label>
+		Name
+		<input
+			type="text"
+			placeholder="Please enter your name"
+			on:keydown={onInputName}
+			bind:value={name}
+		/>
+	</label>
+	<button
+		type="reset"
+		disabled={!name}
+		on:click={resetClicked}
+	>
+		Reset
+	</button>
+</form>
