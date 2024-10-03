6f2a464740f4f479b43270a1c8077dde7d7a2521
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 15:09:12 +0700

    add vcard component

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Navbar from "./sections/Navbar.svelte";
 	import Form from "./components/Form.svelte";
+	import VCard from "./components/VCard.svelte";
 </script>
 
 <Navbar title="vCard Generator" />
 <Form />
+<VCard />
