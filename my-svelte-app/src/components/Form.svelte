a97383b97ffa99694294bfcd297328a1c4589cfd
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 14:55:44 +0700

    update form styling

--- my-svelte-app/src/components/Form.svelte
+++ my-svelte-app/src/components/Form.svelte
@@ -15,9 +15,30 @@
 </script>
 
 <style>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  label {
+    display: flex;
+    flex-grow: 0;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 8px;
+  }
+
+  input {
+    border-radius: 8px;
+    flex-grow: 1;
+    margin-left: 8px;
+  }
+
   .active {
     border-color: #ff9800;
-    box-shadow: 0 1px 3px #f57c00;
     outline: none;
   }
 </style>
