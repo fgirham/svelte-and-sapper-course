d18aaf08a40fd888ed3dea4e95baea85eef7da90
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 14:40:18 +0700

    move form to own component

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,34 +1,7 @@
 <script>
 	import Navbar from "./sections/Navbar.svelte";
-
-	let name = "";
-
-	function onInputName(event) {
-		name = event.target.value;
-	}
-
-	function resetClicked() {
-		name = "";
-	}
+	import Form from "./components/Form.svelte";
 </script>
 
 <Navbar title="vCard Generator" />
-
-<form>
-	<label>
-		Name
-		<input
-			type="text"
-			placeholder="Please enter your name"
-			on:keydown={onInputName}
-			bind:value={name}
-		/>
-	</label>
-	<button
-		type="reset"
-		disabled={!name}
-		on:click={resetClicked}
-	>
-		Reset
-	</button>
-</form>
+<Form />
---
+++ my-svelte-app/src/components/Form.svelte
@@ -0,0 +1,33 @@
+<script>
+	let name = "";
+	let firstInput = "";
+
+	function onInputName(event) {
+		name = event.target.value;
+	}
+
+	function resetClicked() {
+		name = "";
+		firstInput.focus();
+	}
+</script>
+
+<form>
+	<label>
+		Name
+		<input
+			type="text"
+			placeholder="Please enter your name"
+			on:keydown={onInputName}
+			bind:value={name}
+			bind:this={firstInput}
+		/>
+	</label>
+	<button
+		type="reset"
+		disabled={!name}
+		on:click={resetClicked}
+	>
+		Reset
+	</button>
+</form>
