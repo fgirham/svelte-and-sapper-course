395db90bc2a291d213397c4a62b6fe18f774c7c6
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 14:46:06 +0700

    dynamic class binding

--- my-svelte-app/src/components/Form.svelte
+++ my-svelte-app/src/components/Form.svelte
@@ -2,6 +2,8 @@
 	let name = "";
 	let firstInput = "";
 
+  let active = false;
+
 	function onInputName(event) {
 		name = event.target.value;
 	}
@@ -12,12 +14,23 @@
 	}
 </script>
 
+<style>
+  .active {
+    border-color: #ff9800;
+    box-shadow: 0 1px 3px #f57c00;
+    outline: none;
+  }
+</style>
+
 <form>
 	<label>
 		Name
 		<input
 			type="text"
 			placeholder="Please enter your name"
+      class:active
+      on:focus={() => active = true}
+      on:blur={() => active = false}
 			on:keydown={onInputName}
 			bind:value={name}
 			bind:this={firstInput}
