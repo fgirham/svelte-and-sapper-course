6940ce57b9ef1708db9a19a0ba7f492f461d8e21
Faza Ghani Irham <faza.irham@dana.id>
2024-10-03 13:51:58 +0700

    ch01/add-navbar-component

---
+++ my-svelte-app/src/sections/Navbar.svelte
@@ -0,0 +1,18 @@
+
+<script>
+  export let title = 'Default title';
+</script>
+
+<style>
+  nav {
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.5rem 0;
+    background: #333;
+    color: white;
+    text-align: center;
+  }
+</style>
+
+<nav>{title}</nav>
+
